c802c2c7a8430e597a71947ff1087d6aa881a52b
jhs512 <jangka512@gmail.com>
2026-08-12 22:12:14 +0900

    006

---
+++ back/src/test/kotlin/com/back/global/jpa/config/CustomTestPostgreSQLDialect.kt
@@ -0,0 +1,25 @@
+package com.back.global.jpa.config
+
+import org.hibernate.boot.Metadata
+import org.hibernate.boot.model.relational.SqlStringGenerationContext
+import org.hibernate.mapping.Table
+import org.hibernate.tool.schema.internal.StandardTableExporter
+import org.hibernate.tool.schema.spi.Exporter
+
+class CustomTestPostgreSQLDialect : CustomPostgreSQLDialect() {
+    private val tableExporter = object : StandardTableExporter(this) {
+        override fun getSqlCreateStrings(
+            table: Table,
+            metadata: Metadata,
+            context: SqlStringGenerationContext,
+        ): Array<String> {
+            val sqls = super.getSqlCreateStrings(table, metadata, context)
+
+            return sqls.map { sql ->
+                sql.replaceFirst("create table", "create unlogged table")
+            }.toTypedArray()
+        }
+    }
+
+    override fun getTableExporter(): Exporter<Table> = tableExporter
+}
